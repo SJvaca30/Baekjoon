3450bfb00c12ad96eae8eb5a66f64c8437e1bb2e
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-15 09:53:08 +0900

    [level 1] Title: 직사각형 별찍기, Time: 50.46 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,16 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const b = Number(n[0]), a = Number(n[1]);
+    
+    let result = "";
+for (let i = 0; i < a; i++) {
+  for (let j = 0; j < b; j++) {
+    result += "*";
+  }
+  result += "\n";
+}
+
+console.log(result);
+
+});
