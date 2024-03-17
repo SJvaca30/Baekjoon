cb5ba04e53b1b5643e2ee2d2bf500c2c409902e2
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-17 22:02:11 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,8 +1,7 @@
 function solution(sides) {
     let maxNum = Math.max(...sides);
     let sum = sides.reduce( (acc, curr) => curr+acc,0 ) - maxNum
-    let result = 0;
-    result = maxNum < sum ?  1 :  2;
+    let result = maxNum < sum ?  1 :  2;
     return result;
 }
 
