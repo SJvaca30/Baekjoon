2acae817583c116f7c34bebdf9a5cfdd422d93a8
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-20 21:31:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.09 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,8 @@
-function solution(arr)
-{
-    var answer = [arr[0]];
-
-    for(let i = 1; i < arr.length ; i++) {
-        if(arr[i-1] !== arr[i]) answer.push(arr[i]);
+function solution(arr) {
+  return arr.reduce((acc, cur) => {
+    if (acc[acc.length - 1] !== cur) {
+      acc.push(cur);
     }
-
-    return answer;
+    return acc;
+  }, []);
 }
