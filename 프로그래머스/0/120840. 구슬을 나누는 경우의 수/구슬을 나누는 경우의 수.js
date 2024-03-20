ab86f19e631ec2da0f920ae8ef23b720ffe6e48d
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-20 16:37:41 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,10 +1,11 @@
 function solution(n, m) {
-  if (n < m) return 0;
-  if (n === m || m === 0) return 1;
+  if (m > n) return 0; // m > n인 경우는 0으로 처리
+
   let result = 1;
-  for (let i = 0; i < m; i++) {
-    result *= n - i;
-    result /= i + 1;
+  for (let i = 1; i <= m; i++) {
+    result *= n - i + 1; // n - i + 1부터 n까지 곱하기
+    result /= i; // 1부터 i까지 나누기
   }
+
   return result;
 }
