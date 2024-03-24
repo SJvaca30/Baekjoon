264aab7c15dad32ae69d1fd8c88b5b223625005e
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-25 08:57:44 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.88 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,6 +1,6 @@
 function solution(s) {
-  let count = 0; // 제거된 0 개수
-  let depth = 0; // 이진 변환 횟수
+  let count = 0; // 변환 과정에서 제거된 0의 개수를 카운팅
+  let depth = 0; // 이진 변환 횟수를 기록
 
   function convert(str) {
     // 1. 종료 조건
@@ -10,7 +10,7 @@ function solution(s) {
     const newStr = str.replace(/0/g, "");
     const length = newStr.length;
 
-    // 3. 0 개수 카운팅
+    // 3. 제거된 0 개수를 count에 누적
     count += str.length - length;
 
     // 4. 재귀 호출
