b259b21bbf9e20889249b984a8af527653d73e23
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-29 10:32:23 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,24 +1,30 @@
-// 자연수 n의 다음 큰 숫자를 찾는 함수
 function solution(n) {
-  // n을 2진수 문자열로 변환
-  let binaryStr = n.toString(2);
-  // 2진수 문자열에서 '1'의 개수를 카운트
-  let countOne = binaryStr.split('1').length - 1;
-
-  // n+1부터 1000000까지 탐색
-  for (let i = n + 1; i <= 1000000; i++) {
-    // i를 2진수 문자열로 변환
-    let binaryStr2 = i.toString(2);
-    // 2진수 문자열에서 '1'의 개수를 카운트
-    let countOne2 = binaryStr2.split('1').length - 1;
-
-    // n과 2진수로 변환했을 때 1의 개수가 같으면
-    if (countOne2 === countOne) {
-      // 2진수 문자열을 10진수로 변환하여 반환
-      return parseInt(binaryStr2, 2);
+  let nBinary = n.toString(2);
+  let location_01 = nBinary.lastIndexOf("01");
+  
+  if (location_01>0){
+    // 이진수n에 "01"이 존재한다면 즉 101010 1001 같은 경우
+    // 1. 01 을 10으로 바꿔야 함.
+    // 2. 10 뒤에 있는 나머지 101010... 문자에 대해, 000111 이런 식으로 정렬해준다.
+    let newBinary = nBinary.slice(0, location_01) + "10" + nBinary.slice(location_01+2, nBinary.length).split("").sort((a, b)=>(a-b)).join("");
+    return parseInt(newBinary, 2);
+  }else{
+    // 이진수n에 "01"이 없다면 즉, 11110000 이나 1000 같은 경우.
+    if (nBinary.lastIndexOf("1") === 0){
+      // 1이 한개밖에 없다면 다음 큰 수는 이진수n 뒤에 0이 붙은 꼴.
+      let newBinary = nBinary + "0";
+      return parseInt(newBinary, 2);
+    }else{
+      // 1이 여러 개라면 111000 꼴 -> 다음 큰 수는 맨 앞자리에 1을 두고 나머지 1들은 맨 뒤로 몰아두고, 가운데에 0으로 채워두는 꼴.
+      // 다음 큰 숫자의 1의 개수
+      let countOne = nBinary.lastIndexOf("1") + 1;
+      // 다음 큰 숫자의 0의 개수
+      let countZero = nBinary.length + 1 - countOne;
+      let newBinary = "1" + (Array(countZero)).fill(0).join("") + (Array(countOne-1)).fill(1).join("");
+      return parseInt(newBinary, 2);
     }
   }
-
-  // 조건을 만족하는 숫자가 없으면 -1 반환
-  return -1;
 }
+
+
+console.log(solution(78))
