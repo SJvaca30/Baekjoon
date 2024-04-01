454013f25582ab83a7236c8acde163c8989c4ab6
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-01 09:08:50 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,15 +1,12 @@
 function solution(numbers) {
-    let count = 0;
-
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            for (let k = j + 1; k < numbers.length; k++) {
-                if (numbers[i] + numbers[j] + numbers[k] === 0) {
-                    count++;
-                }
-            }
+  return numbers.reduce((count, num1, index1) => {
+    for (let index2 = index1 + 1; index2 < numbers.length; index2++) {
+      for (let index3 = index2 + 1; index3 < numbers.length; index3++) {
+        if (num1 + numbers[index2] + numbers[index3] === 0) {
+          count++;
         }
+      }
     }
-
     return count;
+  }, 0);
 }
