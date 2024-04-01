c92679dc0ea59b0c560c20e80c972553bcf6de5c
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-01 18:30:01 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,17 +1,33 @@
+// function solution(t, p) {
+//   const tLen = t.length;
+//   const pLen = p.length;
+//   const pNum = parseInt(p);
+//   let count = 0;
+
+//   for (let i = 0; i <= tLen - pLen; i++) {
+//     const subStr = t.substring(i, i + pLen);
+
+
+//     if (subNum <= pNum) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+//https://g.co/gemini/share/235e8fa2bc4b
+
 function solution(t, p) {
-  const tLen = t.length;
-  const pLen = p.length;
-  const pNum = parseInt(p);
-  let count = 0;
-
-  for (let i = 0; i <= tLen - pLen; i++) {
-    const subStr = t.substring(i, i + pLen);
-    const subNum = parseInt(subStr);
-
-    if (subNum <= pNum) {
-      count++;
+    let temp = "";
+    let answer = 0;
+    let tL = t.length
+    let pL = p.length
+    for( let i = 0; i < (tL-pL+1); i++) {
+        temp = Number(t.slice(i,i+pL));
+        if (temp <= Number(p)) {
+            answer += 1
+        }
     }
-  }
-
-  return count;
+    return answer
 }
