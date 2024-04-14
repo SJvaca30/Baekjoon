11f510e66d50fa3714d2a57c789b142b72987c7f
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-15 08:50:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,11 +1,17 @@
+// 유클리드 호제법으로 최대공약수 a구하기
 function gcd(a, b) {
-    return b ? gcd(b, a % b) : a;
+  if (b) {
+    return gcd(b, a % b);
+  } else {
+    return a;
+  }
 }
 
+// 최소공배수: 두 수의 곱을 최대공약수로 나눈거
 function lcm(a, b) {
-    return a * b / gcd(a, b);
+  return (a * b) / gcd(a, b);
 }
 
 function solution(arr) {
-    return arr.reduce((a, b) => lcm(a, b));
+  return arr.reduce((a, b) => lcm(a, b));
 }
