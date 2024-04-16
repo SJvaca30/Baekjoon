bcd06642d76a02143cdaffecbd48f65deeb588f2
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-17 08:33:35 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,17 @@
 function solution(array, commands) {
-    let results = [];
-    for (let command of commands) {
-        let [i, j, k] = command;
-        let sliced = array.slice(i-1, j);
-        sliced.sort((a, b) => a - b);
-        results.push(sliced[k-1]);
-    }
-    return results;
+  let results = [];
+
+  for (let command of commands) {
+    // 구조분해할당으로 각각 선언
+    let [i, j, k] = command;
+
+    // 잘라내기
+    let cut = array.slice(i - 1, j);
+    cut.sort((a, b) => a - b);
+
+    results.push(cut[k - 1]);
+  }
+
+  // 결과를 반환합니다.
+  return results;
 }
