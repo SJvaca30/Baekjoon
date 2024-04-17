02cd4126050a11ab8b8faf3a5f5fe66d5bf8aac6
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-17 09:06:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,4 +1,6 @@
+
 function solution(n, arr1, arr2) {
+  // 1. arr1, arr2를 2진수로 변환
   let ARR1 = arr1.map((el) => {
     return Number(el.toString(2));
   });
@@ -6,22 +8,20 @@ function solution(n, arr1, arr2) {
     return Number(el.toString(2));
   });
 
+  // 2. 2진수로 변환한 arr1, arr2를 n자리수로 맞추기
   let ARR3 = [];
-  let result = [];
   for (let i = 0; i < n; i++) {
     ARR3.push((ARR1[i] + ARR2[i]).toString().padStart(n, "0"));
   }
 
+  // 3. 2진수로 변환한 arr1, arr2를 비교하여 결과값 만들기
+  let result = [];
   for (let i = 0; i < n; i++) {
     result.push(
       ARR3[i]
         .split("")
         .map((el) => {
-          if (el !== "0") {
-            return "#";
-          } else {
-            return " ";
-          }
+          return el !== "0" ? "#" : " ";
         })
         .join("")
     );
