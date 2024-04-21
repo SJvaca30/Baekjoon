8a37c051ba58b8626f254d29f19b89a5cf521c96
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-22 08:40:38 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,15 +1,27 @@
+// 푸드 파이트 대회의 음식 배치를 계산하는 함수
 function solution(food) {
-    let result = '';
-    for (let i = 1; i < food.length; i++) {
-        for (let j = 0; j < Math.floor(food[i] / 2); j++) {
-            result += i.toString();
-        }
+  // 결과를 저장할 문자열 초기화
+  let result = "";
+
+  // 음식 배열의 첫 번째 요소부터 중간까지 반복
+  for (let i = 1; i < food.length; i++) {
+    // 각 음식의 절반을 결과 문자열에 추가
+    for (let j = 0; j < Math.floor(food[i] / 2); j++) {
+      result += i.toString();
     }
-    result += '0';
-    for (let i = food.length - 1; i > 0; i--) {
-        for (let j = 0; j < Math.floor(food[i] / 2); j++) {
-            result += i.toString();
-        }
+  }
+
+  // 중간에 물을 추가
+  result += "0";
+
+  // 음식 배열의 마지막 요소부터 중간까지 역순으로 반복
+  for (let i = food.length - 1; i > 0; i--) {
+    // 각 음식의 절반을 결과 문자열에 추가
+    for (let j = 0; j < Math.floor(food[i] / 2); j++) {
+      result += i.toString();
     }
-    return result;
+  }
+
+  // 결과 문자열 반환
+  return result;
 }
